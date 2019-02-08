b81dbe5bcaa33c3ff22f8802c51c69b76019a907
Omkar Mote <omkarmote@Omkar-Mote.local>
2019-02-08 19:03:48 +0530

    Storing todos in the json file

--- todo/src/main/java/com/thoughtworks/todo/TodoController.java
+++ todo/src/main/java/com/thoughtworks/todo/TodoController.java
@@ -4,11 +4,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 @RestController
 public class TodoController {
     @RequestMapping(value = "/todos", method = RequestMethod.GET)
-    public String getTodos() {
-        System.out.println("Aa gaya yaha pe bhi");
-        return "[{'Title: Buy', 'Description': 'Buy clothes this weekend'}, {'Title: Eat', 'Description: Eat pizza tonight}]";
+    public String getTodos() throws IOException {
+        return getTodoContent();
+    }
+
+    private String getTodoContent() throws IOException {
+        String pwd = System.getProperty("user.dir");
+        String filePath = pwd + "/todos.json";
+        List<String> content = Files.readAllLines(Paths.get(filePath));
+        return String.join(" ", content);
     }
 }
