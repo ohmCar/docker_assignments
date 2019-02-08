f26134b2c68624b2d0deba32cfecff196b9ddb08
Omkar Mote <omkarmote@Omkar-Mote.local>
2019-02-08 18:32:02 +0530

    Forwarding todo request to backend and redering todos

--- todo/src/main/java/com/thoughtworks/todo/TodoController.java
+++ todo/src/main/java/com/thoughtworks/todo/TodoController.java
@@ -1,12 +1,14 @@
 package com.thoughtworks.todo;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TodoController {
-    @RequestMapping("/todos")
+    @RequestMapping(value = "/todos", method = RequestMethod.GET)
     public String getTodos() {
-     return "[{'Title: Buy', 'Description': 'Buy clothes this weekend'}, {'Title: Eat', 'Description: Eat pizza tonight}]";
+        System.out.println("Aa gaya yaha pe bhi");
+        return "[{'Title: Buy', 'Description': 'Buy clothes this weekend'}, {'Title: Eat', 'Description: Eat pizza tonight}]";
     }
 }
--- web/server.js
+++ web/server.js
@@ -2,10 +2,18 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const request = require('request');
+
 app.use(express.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
     res.send("Hello");
+});
+
+app.get('/todos', (req, res) => {
+    request.get("http://todoapp:8080/todos", (err, response, data) => {
+        res.send(data);
+    });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
